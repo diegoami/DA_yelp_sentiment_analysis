d83550b15434a31067264a204d8aeca5c3cef5a8
Diego Amicabile <diego.amicabile@gmail.com>
2019-04-07 22:06:37 +0200

    Doing the regressor

--- full_pipeline.py
+++ full_pipeline.py
@@ -1,7 +1,6 @@
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDRegressor
 import pandas as pd
 from nltk.tokenize import word_tokenize
@@ -9,18 +8,30 @@ from string import punctuation
 import logging
 import pickle
 import csv
-ROWS_TOTAL = 10000
+from sklearn.metrics import mean_squared_error
+
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+ROWS_TOTAL = 100000
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 REVIEWS_TOKENS_FILE_IN = '/media/diego/QData/datasets/yelp/review.csv'
 REVIEWS_PIPELINE = '/media/diego/QData/datasets/yelp/review.pipeline'
 
-df = pd.read_csv(REVIEWS_TOKENS_FILE_IN, nrows=ROWS_TOTAL, usecols=['stars'])
-
-
+df = pd.read_csv(REVIEWS_TOKENS_FILE_IN, nrows=ROWS_TOTAL, usecols=['text', 'stars'])
+df = df.sample(frac=1)
+df_train, df_test = train_test_split(df, test_size=0.2)
 
 
 class TokenCountVectorizer(CountVectorizer):
+
+
+    @staticmethod
+    def iterate_pandas_text(df_arg):
+        for index, row in df_arg.text.iteritems():
+            yield row
 
     @staticmethod
     def iterate_ratings():
@@ -40,16 +51,25 @@ class TokenCountVectorizer(CountVectorizer):
         return tokens
 
 
+logging.info("Start .....")
 text_clf = Pipeline([
      ('vect', TokenCountVectorizer(tokenizer=TokenCountVectorizer.to_tokens, max_df=0.5, min_df=5)),
      ('tfidf', TfidfTransformer()),
-     ('clf', SGDRegressor())
+     ('sgd_regressor', SGDRegressor())
 ])
 
-text_clf.fit(TokenCountVectorizer.iterate_ratings(), df.stars)
+logging.info("Fitting Model .....")
+text_clf.fit(TokenCountVectorizer.iterate_pandas_text(df_train), df_train.stars)
+logging.info("Done .....")
 
 with open(REVIEWS_PIPELINE, 'wb') as wmo:
     pickle.dump(text_clf, wmo)
 
-print(text_clf.predict(["This place is great","I hate this place, it sucks","Food is bad, drinks are average, very expensive"]))
+logging.info(f'Predictions: {text_clf.predict(["This place is great","I hate this place, it sucks","Food is bad, drinks are average, very expensive"])}')
 
+test_predicted = text_clf.predict(df_test.text)
+train_predicted = text_clf.predict(df_train.text)
+
+logging.info(f'Train MSE : {mean_squared_error(df_train.stars, train_predicted)}')
+
+logging.info(f'Test MSE : {mean_squared_error(df_test.stars, test_predicted)}')
